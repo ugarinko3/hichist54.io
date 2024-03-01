27073434814080cdbc764787e916761e53a07a1f
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-01 19:41:05 +0700

    site 1-4 slide

--- home_page/views.py
+++ home_page/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request) -> HttpResponse:
-    context: dict = {
-        'title': 'Химчист - фабрики чистки ковров в Новосибирске',
-    }
-    return render(request, 'home_page/index.html', context)
+def index(request):
+    return render(request, 'home_page/index.html')
 
 
 def privacy(request):
