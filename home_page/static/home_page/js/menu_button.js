47e900e099a900545d1339694cc72380ee4e3d5a
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-12 04:06:10 +0700

    menu in slide

--- home_page/static/home_page/js/menu_button.js
+++ home_page/static/home_page/js/menu_button.js
@@ -1,20 +1,31 @@
 function scrollToDivOne() {
     var targetDiv = document.getElementById('slide_one');
     targetDiv.scrollIntoView({ behavior: 'smooth' });
+    offclick();
 }
 function scrollToDivTwo() {
     var targetDiv = document.getElementById('slide_six');
     targetDiv.scrollIntoView({ behavior: 'smooth' });
+    offclick();
 }
 function scrollToDivThree() {
     var targetDiv = document.getElementById('slide_five');
     targetDiv.scrollIntoView({ behavior: 'smooth' });
+    offclick();
 }
 function scrollToDivFour() {
     var targetDiv = document.getElementById('slide_four');
     targetDiv.scrollIntoView({ behavior: 'smooth' });
+    offclick();
 }
 function scrollToDivFive() {
     var targetDiv = document.getElementById('slide_discount');
     targetDiv.scrollIntoView({ behavior: 'smooth' });
+    offclick();
+}
+function offclick(){
+    const windowInnerWidth = window.innerWidth
+    if (windowInnerWidth < 830 ){
+        $('.header_burger,.header_menu').toggleClass('actived');
+    }
 }
