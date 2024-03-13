e062b23fbda022c6a14d0a547c14e0f13ebf8c71
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-13 23:37:45 +0700

    page privacy

--- home_page/static/home_page/js/menu_button.js
+++ home_page/static/home_page/js/menu_button.js
@@ -1,3 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Получаем параметры запроса из URL
+    const urlParams = new URLSearchParams(window.location.search);
+    
+    // Проверяем, существует ли параметр "action" и его значение
+    if (urlParams.has('action')) {
+        const action = urlParams.get('action');
+        // Выполняем функцию в зависимости от значения параметра "action"
+        switch (action) {
+            case 'executeFunction1':
+                scrollToDivOne(); // Замените "yourFunction1" на вашу функцию для Главной
+                break;
+            case 'executeFunction2':
+                scrollToDivTwo(); // Замените "yourFunction2" на вашу функцию для Услуг
+                break;
+            case 'executeFunction3':
+                scrollToDivThree(); // Замените "yourFunction3" на вашу функцию для Калькулятора
+                break;
+            case 'executeFunction4':
+                scrollToDivFour(); // Замените "yourFunction4" на вашу функцию для Этапов
+                break;
+            case 'executeFunction5':
+                scrollToDivFive(); // Замените "yourFunction5" на вашу функцию для Льгот
+                break;
+            default:
+                break;
+        }
+    }
+});
+
 function scrollToDivOne() {
     offclick();
     var targetDiv = document.getElementById('slide_one');
@@ -5,8 +35,10 @@ function scrollToDivOne() {
 }
 function scrollToDivTwo() {
     offclick();
-    var targetDiv = document.getElementById('slide_six');
-    targetDiv.scrollIntoView({ behavior: 'smooth' });
+    setTimeout(function() {
+        var targetDiv = document.getElementById('slide_six');
+        targetDiv.scrollIntoView({ behavior: 'smooth' });
+    });
 }
 function scrollToDivThree() {
     offclick();
--- home_page/static/home_page/js/modal_window_open.js
+++ home_page/static/home_page/js/modal_window_open.js
@@ -1,36 +1,73 @@
-document.getElementById("button_slide").addEventListener("click", openWindow);
-document.getElementById("button_menu_order").addEventListener("click", openWindows);
-document.getElementById("button_menu_order-2").addEventListener("click", openWindow);
-document.getElementById("button_pensia").addEventListener("click", openWindow);
-document.getElementById("button_family").addEventListener("click", openWindow);
-var click_1_close = document.getElementById("close");
-var typeDesign = parseFloat(document.getElementById("button_pensia").value);
+var buttonFunctions = {
+    "button_menu_order": setupMenuOrderListeners,
+    "button_menu_order-2": setupMenuOrder2Listeners,
+    "button_slide": setupSlideListeners,
+    "button_pensia": setupPensiaListeners,
+    "button_family": setupFamilyListeners
+    // Добавьте другие кнопки и функции по необходимости
+};
 
-function FamilyPensia(){
-    var typeDesign = 200;
-}
-
-function openWindow() {
-    document.getElementById("window").classList.add("open");
-}
-function openWindows() {
-    $('.header_burger,.header_menu').toggleClass('actived');
-    document.getElementById("window").classList.add("open");
+// Проходим по каждой паре ключ-значение в объекте buttonFunctions
+for (var buttonId in buttonFunctions) {
+    // Получаем элемент кнопки по идентификатору
+    var button = document.getElementById(buttonId);
     
+    // Если элемент существует, добавляем к нему обработчик события
+    if (button) {
+        // Удаляем существующий обработчик события для данной кнопки
+        button.removeEventListener("click", buttonFunctions[buttonId]);
+        // Добавляем новый обработчик события для данной кнопки
+        button.addEventListener("click", buttonFunctions[buttonId]);
+    }
 }
-click_1_close.onclick = function() {
+function setupMenuOrderListeners() {
+    openWindows();
+}
+
+function setupMenuOrder2Listeners() {
+    openWindow();
+}
+
+function setupSlideListeners() {
+    openWindow();
+}
+
+function setupPensiaListeners() {
+    openWindow();
+}
+
+function setupFamilyListeners() {
+    openWindow();
+}
+
+var clickClose = document.getElementById("close");
+clickClose.onclick = function() {
     document.getElementById("window").classList.remove("open");
-}
+};
+
+
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
-        document.getElementById('window').classList.remove('open')
+        document.getElementById('window').classList.remove('open');
     }
 });
-
 document.querySelector('#window .modal_box').addEventListener('click', event => {
     event._isClickWithInModal = true;
 });
+
+
 document.getElementById('window').addEventListener('click', event =>  {
     if(event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open');
 });
+
+
+function openWindow() {
+    document.getElementById("window").classList.add("open");
+}
+
+function openWindows() {
+    $('.header_burger,.header_menu').toggleClass('actived');
+    $('body').toggleClass('lock');
+    document.getElementById("window").classList.add("open");
+}
