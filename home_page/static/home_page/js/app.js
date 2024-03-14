f0469533dae50bf51d0522c428ffc5a3e9a082cb
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-15 05:54:06 +0700

    email

---
+++ home_page/static/home_page/js/app.js
@@ -0,0 +1,83 @@
+let point = document.querySelectorAll('.point')
+let imageSlider = document.querySelectorAll('.imageSlider')
+let wrapperBox = document.querySelectorAll('.box-info')
+let leftBtn = document.getElementById('leftBtn')
+let rightBtn = document.getElementById('rightBtn')
+
+point[0].classList.add('activeImage')
+imageSlider[0].classList.add('activeImage')
+wrapperBox[0].classList.add('active')
+
+let counter = 0;
+
+for(let i=0; i<point.length; i++){
+    point[i].addEventListener('click',()=>{
+        for(let k = 0; k<imageSlider.length; k++){
+            point[k].classList.remove('activeImage')
+            imageSlider[k].classList.remove('activeImage')
+            wrapperBox[k].classList.remove('active')
+        }
+        counter = i;
+        imageSlider[counter].classList.add('activeImage');
+        point[counter].classList.add('activeImage');
+        wrapperBox[counter].classList.add('active');
+    })
+}
+
+leftBtn.addEventListener('click',()=>{
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+        wrapperBox[k].classList.remove('active')
+    }
+    counter--
+    if (counter <0){
+        counter = imageSlider.length-1
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+    wrapperBox[counter].classList.add('active');
+})
+
+rightBtn.addEventListener('click',()=>{
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+        wrapperBox[k].classList.remove('active')
+    }
+    counter++
+    if (counter >= imageSlider.length){
+        counter = 0
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+    wrapperBox[counter].classList.add('active');
+})
+
+
+function slowSlider () {
+    for(let k = 0; k<imageSlider.length; k++){
+        point[k].classList.remove('activeImage')
+        imageSlider[k].classList.remove('activeImage')
+        wrapperBox[k].classList.remove('active')
+    }
+    counter++
+    if (counter >= imageSlider.length){
+        counter = 0
+    }
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+    wrapperBox[counter].classList.add('active');
+}
+
+let second = 1000*2
+let TimerImage = setInterval(()=>slowSlider(), second)
+
+let blockSlider = document.getElementById('blockSlider')
+blockSlider.addEventListener('mouseover',()=>{
+    clearInterval(TimerImage)
+})
+
+blockSlider.addEventListener('mouseleave',()=>{
+    TimerImage = setInterval(()=>slowSlider(), second)
+})
--- home_page/static/home_page/js/error_input.js
+++ home_page/static/home_page/js/error_input.js
@@ -43,19 +43,101 @@ function validation(form){
 
     return result
 }
+function validation_calculator(form){
+
+    function removeError(input) {
+        const parent = input.parentNode;
+        if (parent.classList.contains('error')) {
+            parent.querySelector('.error-label').remove()
+            parent.classList.remove('error')
+        }
+    }
+    function removeErrorSelector(select) {
+        const parent = select.parentNode;
+        if (select.classList.contains('error')) {
+            parent.querySelector('.error-label').remove()
+            select.classList.remove('error')
+        }
+    }
+    function createError(input, text) {
+        const parent = input.parentNode;
+        const errorLabel = document.createElement('label')
+
+        errorLabel.classList.add('error-label')
+        errorLabel.textContent = text
+
+        parent.classList.add('error')
+
+        parent.append(errorLabel)
+    }
+    function createErrorSelector(select, text) {
+        const parent = select.parentNode
+        const errorLabel = document.createElement('label')
+
+        errorLabel.classList.add('error-label')
+        errorLabel.textContent = text
+        
+        select.classList.add("error");
+
+        parent.append(errorLabel)
+    }
+    
+    
+    let result = true;
+    var selectElements = document.getElementsByClassName("input_choice");
+
+    for (var i = 0; i < selectElements.length; i++) {
+        var selectedValue = selectElements[i].value;
+        removeErrorSelector(selectElements[i])
+        if (selectedValue === "0") {
+            createErrorSelector(selectElements[i], 'Поле не знаполнено!')
+            result = false;
+        }
+    }
+
+    const allInput = form.querySelectorAll('input');
+
+    num = -1;
+    for(const input of allInput) {
+        num += 1;
+        removeError(input)
+        if (num === 6){
+            const secondInput = allInput[num].value;
+            let onlyDigits = secondInput.replace(/\D/g, "");
+            if (onlyDigits.length!==11){
+                createError(allInput[num], 'Поле не знаполнено!')
+                result = false
+            }
+        }
+        if(input.value==''){
+            result = false
+            createError(input, 'Поле не знаполнено!')
+        }
+    }
+    return result
+
+}
+function showModal() {
+    var modal = document.getElementById("successful");
+    modal.classList.add("active"); // Добавляем класс "active", чтобы показать модальное окно
+    setTimeout(function(){
+        modal.classList.remove("active"); // Удаляем класс "active" через 5 секунд
+    }, 5000);
+}
 
 
 
 document.getElementById('form_name_phone').addEventListener('submit', function(event){
     event.preventDefault()
     if (validation(this)==true){
-
-    }
+        document.getElementById("window").classList.remove("open");
+        showModal();
+    };
 });
 document.getElementById('form_calculator').addEventListener('submit', function(event){
     event.preventDefault()
-    if (validation(this)==true){
-
+    if (validation_calculator(this)==true){
+        document.getElementById("window").classList.remove("open");
+        showModal();
     }
 });
-
