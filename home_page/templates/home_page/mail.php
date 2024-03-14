4108900173c8d0e42a9c1861190477d7c711406f
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-15 06:07:18 +0700

    email

--- home_page/templates/home_page/mail.php
+++ home_page/templates/home_page/mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'swiftcleaning@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Fhntv159753.,?'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = '4hKxXCL0CFaEkkPjAGZ2'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
