e297306e88b556e78d07891775759ee45fb8f0b2
ugarinko3@icloud.com <ugarinko3@icloud.com>
2024-03-15 17:37:08 +0700

    email

--- home_page/views.py
+++ home_page/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
 
 def index(request):
     return render(request, 'home_page/index.html')
-
 
 def privacy(request):
     return render(request, 'home_page/privacy.html')
