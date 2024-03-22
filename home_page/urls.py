270ff149c60d052a4fe02ce8f5b44a94324b8d52
ugarinko3 <145273358+ugarinko3@users.noreply.github.com>
2024-03-22 18:00:55 +0700

    Update urls.py

--- home_page/urls.py
+++ home_page/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.forms_calculator, name='index'),
     path('privacy/', views.privacy, name='privacy'),
-    path('', views.forms_calculator, name='forms_calculator'),
-    path('', views.forms_window, name='forms_window'),
+    path('calculator/', views.forms_calculator, name='forms_calculator'),
+    path('window/', views.forms_window, name='forms_window'),
 ]
